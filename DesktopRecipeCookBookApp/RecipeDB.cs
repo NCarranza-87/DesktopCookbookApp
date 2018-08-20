1f0fec85cce7f7ca675fae839fdf533f10f4602e
NCarranza-87 <36281270+NCarranza-87@users.noreply.github.com>
2018-08-19 22:01:45 -0700

    Added CRUD functionality to the application

---
+++ DesktopRecipeCookBookApp/RecipeDB.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesktopRecipeCookBookApp
+{
+    class RecipeDB
+    {
+        public List<recipe> GetAllRecipes()
+        {
+            RecipeContext context = new RecipeContext();
+
+            List<recipe> allRecipes = (from r in context.recipe
+                                       orderby r.recipeTitle ascending
+                                       select r).ToList();
+            return allRecipes;
+        }
+
+        public static void InsertRecipe(recipe r)
+        {
+            RecipeContext context = new RecipeContext();
+
+            //assume Recipe is valid
+            context.recipe.Add(r);
+
+            context.SaveChanges();
+        }
+
+        public static void Update(recipe r)
+        {
+            var context = new RecipeContext();
+
+            //add recipe to object to current context
+            context.recipe.Add(r);
+
+            //Let EF know the book already exists
+            context.Entry(r).State = EntityState.Modified;
+
+            context.SaveChanges();
+        }
+
+        public static void Delete(recipe r)
+        {
+            var context = new RecipeContext();
+
+            context.recipe.Add(r);
+
+            //mark the recipe as deleted
+            context.Entry(r).State = EntityState.Deleted;
+
+            context.SaveChanges();
+        }
+
+    }
+}
