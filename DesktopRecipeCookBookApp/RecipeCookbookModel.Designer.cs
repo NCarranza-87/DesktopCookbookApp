89112a1392440b763f49b6c6afa3dd906071669d
JEANNA KAWAMOTO <kawzie30@live.com>
2018-08-28 16:32:19 -0700

    Corrected new recipe to insert data to database by removing the DBHelper and a method that called the  dbconnection string in dbhelper file. Corrected the RecipeDB  sql script to auto increment recipe id so you could insert . It originally was trying to insert over recipe id 0 Added a message to form that says recipe was saved when user hits submit button and database inserts data.

--- DesktopRecipeCookBookApp/RecipeCookbookModel.Designer.cs
+++ DesktopRecipeCookBookApp/RecipeCookbookModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\NCarr\Documents\DesktopRecipeCookBookApp\DesktopRecipeCookBookApp\RecipeCookbookModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\kawzi\Source\Repos\DesktopCookbookApp\DesktopRecipeCookBookApp\RecipeCookbookModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
